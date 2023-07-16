880bea394d4d8bd2e976e1b6287823b4e82e0cbc
kipling <kip.crossing@gmail.com>
2023-07-16 20:22:04 +1000

    build: add hardhat config for sepolia deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,36 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+dotenv.config();
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY!.toString();
+const ETH_PRIVATE_KEY = process.env.ETH_PRIVATE_KEY!.toString();
+
+module.exports = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ETH_PRIVATE_KEY],
+    },
+  },
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
 };
-
-export default config;
